eeb9821fa088986b73617b0cb939bb61c767717f
Emmanuel Bustos <ema2159@gmail.com>
2022-12-25 12:54:35 -0600

    feat: Implement multiple spheres rendering (Step 2)

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,28 @@ use tinyraytracer::{Camera, Ray, Sphere, TraceObj};
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
+const INTERSECT_LIMIT: f32 = 1000.;
+const BACKGROUND_COLOR: Rgba<u8> = Rgba([51, 178, 204, 255]);
 
-fn render(img: &mut RgbaImage, camera: Camera, obj: &dyn TraceObj) {
+fn scene_intersect(ray: Ray, objs: &Vec<Box<dyn TraceObj>>) -> Option<Rgba<u8>> {
+    let mut intersect_dist = f32::INFINITY;
+    let mut color = Rgba([0, 0, 0, 255]);
+    for obj in objs.iter() {
+        if let Some(intersection) = obj.ray_intersect(&ray) {
+            if intersection < intersect_dist {
+                intersect_dist = intersection;
+                color = Rgba([102, 102, 76, 255]);
+            }
+        }
+    }
+    if intersect_dist < INTERSECT_LIMIT {
+        Some(color)
+    } else {
+        None
+    }
+}
+
+fn render(objs: &Vec<Box<dyn TraceObj>>, camera: Camera, img: &mut RgbaImage) {
     let width = img.width() as f32;
     let height = img.height() as f32;
     let y_fov = f32::tan(camera.fov / 2.);
@@ -23,14 +43,17 @@ fn render(img: &mut RgbaImage, camera: Camera, obj: &dyn TraceObj) {
             let i = ((2. * (x as f32 + 0.5) / width) - 1.) * x_fov;
             let j = -((2. * (y as f32 + 0.5) / height) - 1.) * y_fov;
 
-            if let Some(_) = obj.ray_intersect(&Ray {
-                origin: camera.position,
-                direction: Vector3::new(i, j, -1.).normalize(),
-            }) {
-                img.put_pixel(x, y, Rgba([102, 102, 76, 255]));
+            if let Some(color) = scene_intersect(
+                Ray {
+                    origin: camera.position,
+                    direction: Vector3::new(i, j, -1.).normalize(),
+                },
+                objs,
+            ) {
+                img.put_pixel(x, y, color);
             } else {
-                img.put_pixel(x, y, Rgba([51, 178, 204, 255]));
-            };
+                img.put_pixel(x, y, BACKGROUND_COLOR);
+            }
         }
     }
 }
@@ -43,12 +66,18 @@ fn main() {
         position: Point3::new(0., 0., 0.),
     };
 
-    let sphere = Sphere {
+    let sphere0 = Sphere {
         center: Point3::new(-3., 0., -16.),
         radius: 2.,
     };
+    let sphere1 = Sphere {
+        center: Point3::new(-0.5, 0., -16.),
+        radius: 1.,
+    };
 
-    render(&mut img, camera, &sphere);
+    let spheres: Vec<Box<dyn TraceObj>> = vec![Box::new(sphere0), Box::new(sphere1)];
+
+    render(&spheres, camera, &mut img);
 
     // Rendering window
     let mut window: piston_window::PistonWindow =
